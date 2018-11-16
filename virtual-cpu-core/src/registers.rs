d30586b64b80c56b22aef080041913db9a8b633d
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2018-11-16 15:57:21 -0800

    Add update method to the registers

--- virtual-cpu-core/src/registers.rs
+++ virtual-cpu-core/src/registers.rs
@@ -1,13 +1,20 @@
 pub trait Registers8 {
-    type Name;
+    type Name: Copy;
 
     fn get8(&self, reg: Self::Name) -> u8;
     fn set8(&mut self, reg: Self::Name, val: u8);
+    fn update8(&mut self, reg: Self::Name, updater: impl Fn(u8) -> u8) {
+        self.set8(reg, updater(self.get8(reg)));
+    }
+
 }
 
 pub trait Registers16 {
-    type Name;
+    type Name: Copy;
 
     fn get16(&self, reg: Self::Name) -> u16;
     fn set16(&mut self, reg: Self::Name, val: u16);
+    fn update16(&mut self, reg: Self::Name, updater: impl Fn(u16) -> u16) {
+        self.set16(reg, updater(self.get16(reg)));
+    }
 }
